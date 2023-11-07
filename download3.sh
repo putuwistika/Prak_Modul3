96738a09e82e28d3c19bb96cf6f8e3d0507fab28
I Putu Ferry Wistika <144299403+putuwistika@users.noreply.github.com>
2023-11-07 13:59:34 +0700

    Update download3.sh

--- download3.sh
+++ download3.sh
@@ -1,26 +1,99 @@
 #!/bin/bash
 
+
 # Membersihkan layar
 clear
+# Reset
+Color_Off='\033[0m'       # Text Reset
+
+# Regular Colors
+Black='\033[0;30m'        # Black
+Red='\033[0;31m'          # Red
+Green='\033[0;32m'        # Green
+Yellow='\033[0;33m'       # Yellow
+Blue='\033[0;34m'         # Blue
+Purple='\033[0;35m'       # Purple
+Cyan='\033[0;36m'         # Cyan
+White='\033[0;37m'        # White
+
+# Bold
+BBlack='\033[1;30m'       # Black
+BRed='\033[1;31m'         # Red
+BGreen='\033[1;32m'       # Green
+BYellow='\033[1;33m'      # Yellow
+BBlue='\033[1;34m'        # Blue
+BPurple='\033[1;35m'      # Purple
+BCyan='\033[1;36m'        # Cyan
+BWhite='\033[1;37m'       # White
+
+# Underline
+UBlack='\033[4;30m'       # Black
+URed='\033[4;31m'         # Red
+UGreen='\033[4;32m'       # Green
+UYellow='\033[4;33m'      # Yellow
+UBlue='\033[4;34m'        # Blue
+UPurple='\033[4;35m'      # Purple
+UCyan='\033[4;36m'        # Cyan
+UWhite='\033[4;37m'       # White
+
+# Background
+On_Black='\033[40m'       # Black
+On_Red='\033[41m'         # Red
+On_Green='\033[42m'       # Green
+On_Yellow='\033[43m'      # Yellow
+On_Blue='\033[44m'        # Blue
+On_Purple='\033[45m'      # Purple
+On_Cyan='\033[46m'        # Cyan
+On_White='\033[47m'       # White
+
+# High Intensity
+IBlack='\033[0;90m'       # Black
+IRed='\033[0;91m'         # Red
+IGreen='\033[0;92m'       # Green
+IYellow='\033[0;93m'      # Yellow
+IBlue='\033[0;94m'        # Blue
+IPurple='\033[0;95m'      # Purple
+ICyan='\033[0;96m'        # Cyan
+IWhite='\033[0;97m'       # White
+
+# Bold High Intensity
+BIBlack='\033[1;90m'      # Black
+BIRed='\033[1;91m'        # Red
+BIGreen='\033[1;92m'      # Green
+BIYellow='\033[1;93m'     # Yellow
+BIBlue='\033[1;94m'       # Blue
+BIPurple='\033[1;95m'     # Purple
+BICyan='\033[1;96m'       # Cyan
+BIWhite='\033[1;97m'      # White
+
+# High Intensity backgrounds
+On_IBlack='\033[0;100m'   # Black
+On_IRed='\033[0;101m'     # Red
+On_IGreen='\033[0;102m'   # Green
+On_IYellow='\033[0;103m'  # Yellow
+On_IBlue='\033[0;104m'    # Blue
+On_IPurple='\033[0;105m'  # Purple
+On_ICyan='\033[0;106m'    # Cyan
+On_IWhite='\033[0;107m'   # White
 
 # Menyimpan tanggal dan waktu awal
 start_time=$(date +"%Y-%m-%d %T")
-echo "======================================================================================================================="
+echo "${BRed}=======================================================================================================================${Color_Off}"
 
-echo "                  ┏━━┓           ┏┓     ┏┓     ┏┓                    ┏━━┓              ┏┓"
+echo "${BIGreen}                  ┏━━┓           ┏┓     ┏┓     ┏┓                    ┏━━┓              ┏┓"
 echo "                  ┃┏┓┃           ┃┃    ┏┛┗┓    ┃┃                    ┃┏┓┃             ┏┛┃"
 echo "                  ┃┗┛┃  ┏━┓ ┏━━┓ ┃┃┏┓  ┗┓┏┛ ┏┓ ┃┃┏┓ ┏┓┏┓ ┏┓┏┓   ┏┓┏┓ ┃┗┛┃ ┏━━┓ ┏━┓    ┗┓┃"
 echo "                  ┃┏━┛  ┃┏┛ ┃┏┓┃ ┃┗┛┛   ┃┃  ┣┫ ┃┗┛┛ ┃┃┃┃ ┃┗┛┃   ┃┗┛┃ ┃┏━┛ ┃┏━┛ ┃┏┓┓    ┃┃"
 echo "                  ┃┃    ┃┃  ┃┏┓┃ ┃┏┓┓   ┃┗┓ ┃┃ ┃┏┓┓ ┃┗┛┃ ┃┃┃┃   ┃┃┃┃ ┃┃   ┃┗━┓ ┃┃┃┃   ┏┛┗┓"
-echo "                  ┗┛    ┗┛  ┗┛┗┛ ┗┛┗┛   ┗━┛ ┗┛ ┗┛┗┛ ┗━━┛ ┗┻┻┛   ┗┻┻┛ ┗┛   ┗━━┛ ┗┛┗┛   ┗━━┛"
+echo "                  ┗┛    ┗┛  ┗┛┗┛ ┗┛┗┛   ┗━┛ ┗┛ ┗┛┗┛ ┗━━┛ ┗┻┻┛   ┗┻┻┛ ┗┛   ┗━━┛ ┗┛┗┛   ┗━━┛ ${Color_Off}"
 
 
 
-echo "======================================================================================================================="
+echo "${BRed}=======================================================================================================================${Color_Off}"
 echo " "
-echo "                              ASISTEN PRAKTIKUM MPCN I 2023 BERSAMA KALIAN"
+echo "                              ${IPurple} ASISTEN PRAKTIKUM MPCN I 2023 BERSAMA KALIAN ${Color_Off}"
 echo " "
-echo "                              *******************************************"
+echo " ${BCyan}                              *******************************************"
 echo "                                      JALAN JALAN NAIK KERETA"
 echo "                                      NAIKNYA DI GERBONG TIGA,"
 echo "                                      SEMANGAT DOWNLOAD DATA"
@@ -29,11 +102,10 @@ echo " "
 echo "                                      BTW SIZE DATANYA 1.4 GB"
 echo "                         PASTIKAN SINYAL KAMU KUENCENG & STORAGE KAMU TERSEDIA!!!"
 echo " "
-echo "                               *******************************************"
-echo " "
+echo "                               ******************************************* ${Color_Off}"
+echo "${BGreen} "
 echo "======================================================================================================================="
 echo "                                          DOWNLOADING........"
-echo "                                        ---TUNGGU YAH SAYANG---"
 echo "======================================================================================================================="
 echo " "
 
@@ -45,7 +117,7 @@ wget --load-cookies /tmp/cookies.txt "https://drive.google.com/uc?export=downloa
 
 echo "======================================================================================================================="
 echo "                                  UNPACKING DATA........"
-echo "======================================================================================================================="
+echo "======================================================================================================================= ${Color_Off}"
 
 # Memeriksa apakah 'unzip' sudah terinstal
 if ! command -v unzip &> /dev/null
@@ -67,19 +139,26 @@ echo "End Time: $end_time" >> download.log
 
 # Hapus file yang tidak diperlukan
 rm MPCN_modul3.zip download_modul3.sh
-mv MPCN_modul3 MODUL3
 rm download.log
 rm unzip.log
 
-mv MODUL3 ../
-cd ..
-rm -rf Prak_Modul3
 
 # Membersihkan layar
 clear
 
+
+echo "${BRed}=======================================================================================================================${Color_Off}"
+
+echo "${BIGreen}                  ┏━━┓           ┏┓     ┏┓     ┏┓                    ┏━━┓              ┏┓"
+echo "                  ┃┏┓┃           ┃┃    ┏┛┗┓    ┃┃                    ┃┏┓┃             ┏┛┃"
+echo "                  ┃┗┛┃  ┏━┓ ┏━━┓ ┃┃┏┓  ┗┓┏┛ ┏┓ ┃┃┏┓ ┏┓┏┓ ┏┓┏┓   ┏┓┏┓ ┃┗┛┃ ┏━━┓ ┏━┓    ┗┓┃"
+echo "                  ┃┏━┛  ┃┏┛ ┃┏┓┃ ┃┗┛┛   ┃┃  ┣┫ ┃┗┛┛ ┃┃┃┃ ┃┗┛┃   ┃┗┛┃ ┃┏━┛ ┃┏━┛ ┃┏┓┓    ┃┃"
+echo "                  ┃┃    ┃┃  ┃┏┓┃ ┃┏┓┓   ┃┗┓ ┃┃ ┃┏┓┓ ┃┗┛┃ ┃┃┃┃   ┃┃┃┃ ┃┃   ┃┗━┓ ┃┃┃┃   ┏┛┗┓"
+echo "                  ┗┛    ┗┛  ┗┛┗┛ ┗┛┗┛   ┗━┛ ┗┛ ┗┛┗┛ ┗━━┛ ┗┻┻┛   ┗┻┻┛ ┗┛   ┗━━┛ ┗┛┗┛   ┗━━┛ ${Color_Off}"
+
+
 echo " "
-echo "======================================================================================================================="
+echo "${BCyan} ======================================================================================================================="
 echo "                                           YEYYY SUDAH SELESAI"
 echo "======================================================================================================================="
 echo "                                      JALAN JALAN KE PANTAI BARU"
@@ -90,5 +169,5 @@ echo "                                      SELAMAT MENGERJAKAN PRAKTIKUM"
 echo " "
 echo "                          @SALAM HANGAT ASISTEN PRAKTIKUM MPCN I 2023 ALA ALA DUM DUM"
 echo "                                        TRAKTIR TEH POCI DONGS HEHE"
-echo "======================================================================================================================="
+echo "======================================================================================================================= ${Color_Off}"
 cd ../
